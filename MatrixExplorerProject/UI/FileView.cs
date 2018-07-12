6f1a26f62e56249bbfb5bf2bc47cc8e1eb2b0b84
NaohoroHonda <39308876NaohiroHonda@users.noreply.github.com>
2018-07-12 20:53:31 +0900

    作業保存

---
+++ MatrixExplorerProject/UI/FileView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixExplorerProject.UI
+{
+    public partial class FileView : Form
+    {
+        public FileView()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FileView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
